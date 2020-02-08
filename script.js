9b5a4c20636177f60cef21280dcdfe38f7f00702
Glitch (kickstart-the-movement) <none>
2020-02-08 00:59:15 +0000

    🌴💷 Updated with Glitch

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-$(".CreateButton").click(function(){
+$(".CreateButton").click(function() {
   var title = $(".title").val();
-  localStorage.setItem("title", $(".title").val());
-  
-  
-  var description = $(".description").val();
-  localStorage.setItem("description", $(".description").val());
-  
-  
   var date = $(".date").val();
-  localStorage.setItem("date", $(".date").val());
-  
-  
-  var image = $(".image").val();
-  localStorage.setItem("image", $(".image").val());
-  
-  console.log(localStorage.getItem("title"));
-  console.log(title);
-  console.log(description);
-  console.log(date);
-  window.location.href ="/Popular.html"
-  
-  var title = localStorage.getItem("title");
-  console.log(title);
-  $(".titleEvent4").text(title);
-  $(".descriptionEvent4").text(description);
-  $(".dateEvent4").text(date);
+  var description = $(".description").val();
+  $(".title4").text(title);
+  $(".date4").text(date);
+  $(".description4").text(description);
 });
